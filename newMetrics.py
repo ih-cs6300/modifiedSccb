a81523cdbec27b3ad2ee44efec93ab79d3ef7a4c
Ira Harmon <iharmon@ihmc.us>
2020-02-14 13:17:29 -0500

    train works with fia-daymet extended data set; acc 93%

---
+++ newMetrics.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn import metrics
+import warnings
+
+#ignore warnings about depracated packages
+warnings.filterwarnings("ignore")
+
+yobs=pd.read_csv("~/test/ccb-id/ccbid/support_files/ResultsTestSet.csv")
+df = pd.read_csv("./ecodse-results.csv")
+idx = df.groupby(['crown'])['probability'].transform(max) == df['probability']
+df = df[idx]
+full_data = df.merge(yobs, left_on="crown", right_on="itcID")
+
+print "Confusion Matrix"
+print metrics.confusion_matrix(full_data["SpeciesID"], full_data["species"])
+print
+print
+print "Macro F1: " + str(metrics.f1_score(full_data["SpeciesID"], full_data["species"], average='macro'))
+print
+print
+print "Micro F1: " + str(metrics.f1_score(full_data["SpeciesID"], full_data["species"], average='micro'))
+splist = list(df['species'].unique())
+splist.sort()
+print
+print
+print metrics.classification_report(full_data["SpeciesID"], full_data["species"], target_names=splist)
